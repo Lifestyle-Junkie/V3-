3b61ace15168b502710999b00f296a0b9e972af6
Stranded X <Nick82186@gmail.com>
2026-09-12 01:54:23 -0400

    Refactor renderHolds to include sparkHtml

--- capital.js
+++ capital.js
@@ -62,20 +62,27 @@ function drawCapChart() {
     '<circle cx="' + lx + '" cy="' + ly + '" r="4" fill="#3ee07a"/>' +
     "</svg>";
 }
+function sparkHtml() {
+  var n = 12, html = '<span class="cap-spark"><span class="candle-chart">';
+  for (var i = 0; i < n; i++) html += '<span class="candle"></span>';
+  return html + "</span></span>";
+}
 function renderHolds() {
   const box = document.getElementById("capHolds");
   if (!box) return;
-  box.innerHTML = CAP_HOLDINGS.map(function (h) {
-    const src = CAP_LOGOS[h.t] || "";
-    return (
-      '<div class="cap-hold">' +
-      '<img class="tlogo" alt="' + h.t + '" src="' + src + '" />' +
-      '<span class="tk">' + h.t + "</span>" +
-      '<span class="pv">' + money(h.val) + "</span>" +
-      '<span class="chg">+' + h.chg.toFixed(2) + "%</span>" +
-      "</div>"
-    );
-  }).join("");
+  box.innerHTML =
+    '<div class="cap-hold cap-hold-h"><span></span><span>Ticker</span><span>Price</span><span>Change</span></div>' +
+    CAP_HOLDINGS.map(function (h) {
+      const src = CAP_LOGOS[h.t] || "";
+      return (
+        '<div class="cap-hold">' +
+        '<img class="tlogo" alt="' + h.t + '" src="' + src + '" />' +
+        '<span class="tk">' + h.t + "</span>" +
+        '<span class="pv">' + money(h.val) + "</span>" +
+        '<span class="chg">+' + h.chg.toFixed(2) + "% " + sparkHtml() + "</span>" +
+        "</div>"
+      );
+    }).join("");
   box.querySelectorAll("img.tlogo").forEach(function (img) {
     img.addEventListener("error", function () {
       const letter = document.createElement("span");
